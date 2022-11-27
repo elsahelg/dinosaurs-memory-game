0927e4a1ca58cf78aa06cd0c0ba32d68221c06c7
Elsa Helg <elsahelg@gmail.com>
2022-11-27 00:38:27 +0000

    Add event listener for DOM content loaded and commented out the main functions for game

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  //images for cards
+
+  //generate memory board
+  function generateBoard() 
+
+  //flip cards
+  function flipCard()
+
+  //check chosen cards for match
+  function cardsMatch()
+
+})
