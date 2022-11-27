3148d6c626d410921970555317f563b0c38deaca
Elsa Helg <elsahelg@gmail.com>
2022-11-27 20:56:57 +0000

    Add function to randomize order of cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -51,7 +51,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     ]
 
-   
+    cardArray.sort (() => 0.5 - Math.random())
 
     const grid = document.querySelector('.grid')
     const scoreDisplay = document.querySelector('.score')
