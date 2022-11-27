24707bc931d83f73deed94bdebfe552268294e62
Elsa Helg <elsahelg@gmail.com>
2022-11-27 02:59:37 +0000

    Add function to generate cards to memory board, create img element to html document through grid. Call function to generate memory board

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,75 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  //images for cards
+    //images for cards
+    const cardArray = [
+        {
+            name: 'acanthopholis',
+            img: 'assets/images/acanthopholis.jpg'
+        },
+        {
+            name: 'acanthopholis',
+            img: 'assets/images/acanthopholis.jpg'
+        },
+        {
+            name: 'parasaurolophus',
+            img: 'assets/images/parasaurolophus.jpg'
+        },
+        {
+            name: 'parasaurolophus',
+            img: 'assets/images/parasaurolophus.jpg'
+        },
+        {
+            name: 'pterodactyl',
+            img: 'assets/images/pterodactyl.jpg'
+        },
+        {
+            name: 'pterodactyl',
+            img: 'assets/images/pterodactyl.jpg'
+        },
+        {
+            name: 'stegosaurus',
+            img: 'assets/images/stegosaurus.jpg'
+        },
+        {
+            name: 'stegosaurus',
+            img: 'assets/images/stegosaurus.jpg'
+        },
+        {
+            name: 'triceratops',
+            img: 'assets/images/triceratops.jpg'
+        },
+        {
+            name: 'triceratops',
+            img: 'assets/images/triceratops.jpg'
+        },
+        {
+            name: 'tyrannosaurusrex',
+            img: 'assets/images/tyrannosaurusrex.jpg'
+        },
+        {
+            name: 'tyrannosaurusrex',
+            img: 'assets/images/tyrannosaurusrex.jpg'
+        }
+    ]
 
-  //generate memory board
-  function generateBoard() 
+    const grid = document.querySelector('.grid')
 
-  //flip cards
-  function flipCard()
+    //generate memory board
+    function generateBoard() {
+        for (let i = 0; i < cardArray.length; i++) {
+            var card = document.createElement('img')
+            card.setAttribute('src', 'assets/images/card.jpeg')
+            card.setAttribute('data-id', i)
+            // card.addEventListener('click', flipCard)
+            grid.appendChild(card)
+        }
+    }
 
-  //check chosen cards for match
-  function cardsMatch()
+    generateBoard()
+
+    //flip cards   function flipCard()
+  
+    //check chosen cards for match     function cardsMatch()
+
 
 })
