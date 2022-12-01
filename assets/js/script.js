e90dc9bc3ee9fa7e11269537d09ce91120b38dac
Elsa Helg <elsahelg@gmail.com>
2022-12-01 18:00:40 +0000

    Add semicolon where missing after running code in JSHint

--- assets/js/script.js
+++ assets/js/script.js
@@ -63,46 +63,46 @@ document.addEventListener('DOMContentLoaded', () => {
     //generate memory board
     function generateBoard() {
         for (let i = 0; i < cardArray.length; i++) {
-            var card = document.createElement('img')
-            card.setAttribute('src', 'assets/images/card.jpeg')
-            card.setAttribute('data-id', i)
-            card.addEventListener('click', flipCard)
-            grid.appendChild(card)
+            var card = document.createElement('img');
+            card.setAttribute('src', 'assets/images/card.jpeg');
+            card.setAttribute('data-id', i);
+            card.addEventListener('click', flipCard);
+            grid.appendChild(card);
         }
-    };
+    }
 
     //flip cards   
     function flipCard() {
-        var cardId = this.getAttribute('data-id')
-        cardsClicked.push(cardArray[cardId].name)
-        cardsClickedId.push(cardId)
-        this.setAttribute('src', cardArray[cardId].img)
+        var cardId = this.getAttribute('data-id');
+        cardsClicked.push(cardArray[cardId].name);
+        cardsClickedId.push(cardId);
+        this.setAttribute('src', cardArray[cardId].img);
         if (cardsClicked.length === 2) {
-            setTimeout(checkMatch, 400)
+            setTimeout(checkMatch, 400);
         }
-    };
+    }
 
     //check chosen cards for match     
     function checkMatch() {
-        var cards = document.querySelectorAll('img')
-        const firstPickId = cardsClickedId[0]
-        const secondPickId = cardsClickedId[1]
+        var cards = document.querySelectorAll('img');
+        const firstPickId = cardsClickedId[0];
+        const secondPickId = cardsClickedId[1];
         if (cardsClicked[0] === cardsClicked[1]) {
-            textAlertDisplay.textContent = "It's a match! Keep going!"
-            cardsMatch.push(cardsClicked)
+            textAlertDisplay.textContent = "It's a match! Keep going!";
+            cardsMatch.push(cardsClicked);
         } else {
-            textAlertDisplay.textContent = "Sorry, no match! Try again!"
-            cards[firstPickId].setAttribute('src', 'assets/images/card.jpeg')
-            cards[secondPickId].setAttribute('src', 'assets/images/card.jpeg')
+            textAlertDisplay.textContent = "Sorry, no match! Try again!";
+            cards[firstPickId].setAttribute('src', 'assets/images/card.jpeg');
+            cards[secondPickId].setAttribute('src', 'assets/images/card.jpeg');
         }
-        cardsClicked = []
-        cardsClickedId = []
-        scoreDisplay.textContent = cardsMatch.length
+        cardsClicked = [];
+        cardsClickedId = [];
+        scoreDisplay.textContent = cardsMatch.length;
         if (cardsMatch.length === 6) {
-            textAlertDisplay.textContent = 'Congratulations! You found all twins!'
+            textAlertDisplay.textContent = 'Congratulations! You found all twins!';
         }
-    };
+    }
 
     generateBoard();
 
-})
+});
